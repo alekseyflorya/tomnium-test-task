1fdad6fca6d8ce38ccd86164e9c7321177ff36b6
alekseyflorya <aleksey.florya@gmail.com>
2020-08-23 01:27:52 +0300

    23-08-2020

---
+++ build/precache-manifest.a6460bc06dd913d129aeecfae3c9647d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29668d92c40adc017bee04b08a9298ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d97e15ba821510fc3267",
+    "url": "/static/css/main.0d6bab77.chunk.css"
+  },
+  {
+    "revision": "8980a3b5ef968f19e204",
+    "url": "/static/js/2.bd9964a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd9964a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97e15ba821510fc3267",
+    "url": "/static/js/main.86bc8263.chunk.js"
+  },
+  {
+    "revision": "a2abdf44820338c288b0",
+    "url": "/static/js/runtime-main.96b26d50.js"
+  }
+]);
